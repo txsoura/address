c0df0b6152c29b81c39e4584882a009de159a1e2
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-11 13:09:28 -0300

    fix(resource): fix address resource name

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserCollection extends JsonResource
+class AddressResource extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
