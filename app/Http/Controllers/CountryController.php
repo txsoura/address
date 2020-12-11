6e2df53dd30a00765be7f221a47e9030d15ae703
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-11 01:53:31 -0300

    feat(controller): create country controller

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CountryResource;
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CountryResource::collection(Country::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'code' => 'required|string|unique:countries'
+        ]);
+
+        $country = Country::create($request->all());
+
+        return new CountryResource($country, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Country $country)
+    {
+        return new CountryResource($country, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Country $country)
+    {
+        $request->validate([
+            'name' => 'string',
+            'code' => 'string|unique:countries'
+        ]);
+
+        $country->update($request->all());
+
+        return new CountryResource($country, 202);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Country $country)
+    {
+        $country->delete();
+        return response()->json(['message' => trans('message.deleted')], 204);
+    }
+}
