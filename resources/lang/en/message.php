45a4cf3d19ce7f59d5ba4862dda8a0a45c479122
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-11 14:35:27 -0300

    feat(lang): update en dictionary

---
+++ resources/lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'route_not_found' => 'Route not found',
+    'entry_not_found' => 'Entry for :model not found',
+    'not_found' => 'Not found',
+    'deleted' => 'Deleted successfully',
+];
