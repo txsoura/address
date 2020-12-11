1efaa1c57315aa3e5a77ab546d50be5fc999973f
Victor Tesoura Júnior <txsoura@yahoo.com>
2020-12-11 14:36:32 -0300

    feat(exception): remove unused import

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Validation\UnauthorizedException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Throwable;
 
