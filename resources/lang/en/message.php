f35ca1ff49644ab33dc5a1c01da5e677055880bb
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-30 21:25:02 -0300

    feat(lang): create en message dictionary words

---
+++ resources/lang/en/message.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'route_not_found' => 'Route not found',
+    'not_found' => 'Not found',
+    'deleted' => 'Deleted successfully',
+    'permission_not_found' => 'Permission not found',
+    'no_permission' => 'You are not allowed to perform this operation',
+];
