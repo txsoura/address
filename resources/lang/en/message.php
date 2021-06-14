f2b3239709a7c0b4d76a3a5af7fb79668313acbb
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-06-14 17:02:54 -0300

    feat(lang): update en message dictionary

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -2,6 +2,8 @@
 
 return [
     'route_not_found' => 'Route not found',
+    'entry_not_found' => 'Entry for :model not found',
+    'relation_not_found' => 'Relation not found',
     'not_found' => 'Not found',
     'deleted' => 'Deleted successfully',
     'permission_not_found' => 'Permission not found',
